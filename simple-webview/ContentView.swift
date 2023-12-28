5329a1aad4bb0a3d8397cc558776da1fd3e5716b
박주렁 <jurung@pplink.net>
2023-12-28 15:21:24 +0900

    Initial Commit

---
+++ simple-webview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  simple-webview
+//
+//  Created by 박주렁 on 12/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ simple-webview/simple_webviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_webviewApp.swift
+//  simple-webview
+//
+//  Created by 박주렁 on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct simple_webviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
